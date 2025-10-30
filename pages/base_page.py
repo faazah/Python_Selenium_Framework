119a46ec5f8ad2ddd80fa58538e1274a91a04fa5
faazah <faizan8t8@gmail.com>
2025-10-30 14:43:15 +0530

    Added the Allure Reporting with the help of allure-pytest and maintained a folder reports in which the reports will be saved

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,5 @@
 import logging
+import allure
 
 from selenium.webdriver.support.expected_conditions import visibility_of_element_located
 from selenium.webdriver.support.wait import WebDriverWait
@@ -20,6 +21,7 @@ class BasePage:
         self.wait=WebDriverWait(self.driver, self.timeout)
         self.logger=logging.getLogger(__name__)  #Get logger for this module
 
+    @allure.step("Navigating to the base url: {base_url}")
     def navigate_to(self, base_url):
         try:
             self.driver.get(base_url)
@@ -28,6 +30,7 @@ class BasePage:
             self.logger.error(f"Failed to navigate to {base_url}")
             raise
 
+    @allure.step("Finding element by locator: {locator}")
     def find_element(self, locator):
         try:
             return self.wait.until(
@@ -37,36 +40,43 @@ class BasePage:
             self.logger.error(f"Failed to find element {locator} within {self.timeout} seconds")
             raise
 
+    @allure.step("Clicking the element with locator: {locator}")
     def click(self, locator):
         element=self.find_element(locator)
         element.click()
         self.logger.info(f"Clicked the element {locator}")
 
+    @allure.step("Entering the text: {text} with locator: {locator}")
     def enter_text(self, locator, text):
         element=self.find_element(locator)
         element.clear()
         element.send_keys(text)
         self.logger.info(f"Entered text {text} on element {locator}")
 
+    @allure.step("Getting the text from the element having locator: {locator}")
     def get_text(self, locator):
         element=self.find_element(locator)
         text = element.text
         self.logger.info(f"Retrieved text '{text}' from element: {locator}")
         return text
 
+    @allure.step("Getting attribute value of attribute :{attr} with locator: {locator}")
     def get_attribute_value(self, locator, attr):
         element=self.find_element(locator)
         return element.get_attribute(attr)
 
+    @allure.step("Checking whether the element is displayed or not with locator: {locator}")
     def is_element_displayed(self, locator):
         element=self.find_element(locator)
         return element.is_displayed()
 
+    @allure.step("Getting the title of the page")
     def get_title(self):
         title=self.driver.title
         self.logger.info(f"Current title is {title}")
         return title
 
+    @allure.step("Scrolling to an element with locator: {locator}")
     def scroll_to_element(self, locator):
         try:
             action_obj=ActionChains(self.driver)
@@ -76,6 +86,7 @@ class BasePage:
             self.logger.error(f"Failed to scroll to element {locator}")
             raise
 
+    @allure.step("Checking the visibility of the element having locator: {locator}")
     def is_visible(self, locator):
         try:
             self.wait.until(
--- tests/test_crm_admin_login.py
+++ tests/test_crm_admin_login.py
@@ -1,3 +1,5 @@
+import allure
+
 from selenium import webdriver
 
 from config.environment import Environment
@@ -12,17 +14,18 @@ driver.implicitly_wait(5)
 invalid_username="admin123"
 invalid_password="admin123"
 
+@allure.feature("Authentication")
+@allure.story("Admin Login")
 class TestAdminLogin:
     """
     Test class for login functionality against Small CRM app.
     """
-
+    @allure.title("Test Successful login with valid credentials")
+    @allure.severity(allure.severity_level.CRITICAL)
     def test_successful_login(self):
         """
         Test Case: Verify successful login using valid Credentials from config.yaml file
         """
-        print("Testing successful login")
-
         ##---Initializing Login Page and variables
         login_pg = AdminLoginPage(driver)
         env = Environment('practice')
@@ -30,43 +33,49 @@ class TestAdminLogin:
         username = env.get_username()
         password = env.get_password()
 
-        print(f"Navigating to the base_url {base_url}")
-        login_pg.navigate_to(base_url)
-        assert login_pg.is_element_displayed(login_pg.ADMIN_LOGIN_PAGE_HEADER), "Login Page did not load properly"
+        # print(f"Navigating to the base_url {base_url}")
+        with allure.step(f"Navigate to the Admin Login Page: {base_url}"):
+            login_pg.navigate_to(base_url)
+            assert login_pg.is_element_displayed(login_pg.ADMIN_LOGIN_PAGE_HEADER), "Login Page did not load properly"
 
-        print(f"Logging in with Valid Credentials")
-        login_pg.admin_login(username, password)
-        assert login_pg.is_element_displayed(login_pg.DASHBOARD_PAGE_HEADER)
+        # print(f"Logging in with Valid Credentials")
+        with allure.step(f"Perform login with valid Credentials"):
+            login_pg.admin_login(username, password)
+            assert login_pg.is_element_displayed(login_pg.DASHBOARD_PAGE_HEADER)
 
-        print(f"Verifying successful login and Page Title")
-        assert login_pg.is_login_successful(), "Login was not successful, dashboard page did not load"
+        # print(f"Verifying successful login and Page Title")
+        with allure.step(f"Verify successful login and page title"):
+            assert login_pg.is_login_successful(), "Login was not successful, dashboard page did not load"
 
-        expected_title="CRM | Admin Dashboard"
-        actual_title=login_pg.get_title()
-        assert actual_title == expected_title, f"Actual title {actual_title} did not match expected title {expected_title}"
+            expected_title="CRM | Admin Dashboard"
+            actual_title=login_pg.get_title()
+            assert actual_title == expected_title, f"Actual title {actual_title} did not match expected title {expected_title}"
 
-
+    @allure.title(f"Test Login failure with invalid credentials")
+    @allure.severity(allure.severity_level.NORMAL)
     def test_invalid_login(self):
         """
         Test Case: Verify invalid login using invalid Credentials.
         """
-        print("Testing invalid login")
 
         ##---Initializing Login Page and Variables
         login_pg = AdminLoginPage(driver)
         env = Environment('practice')
         base_url = env.get_base_url()
 
-        print(f"Navigating to the base_url {base_url}")
-        login_pg.navigate_to(base_url)
-        assert login_pg.is_element_displayed(login_pg.ADMIN_LOGIN_PAGE_HEADER), "Login Page did not load"
+        # print(f"Navigating to the base_url {base_url}")
+        with allure.step(f"Navigating to the login page: {base_url}"):
+            login_pg.navigate_to(base_url)
+            assert login_pg.is_element_displayed(login_pg.ADMIN_LOGIN_PAGE_HEADER), "Login Page did not load"
 
-        print(f"Logging in with Invalid Credentials")
-        login_pg.admin_login(invalid_username, invalid_password)
+        with allure.step(f"Perform login with invalid credentials"):
+            # print(f"Logging in with Invalid Credentials")
+            login_pg.admin_login(invalid_username, invalid_password)
 
-        print(f"Verifying invalid login and Page Title")
-        expected_error_msg="*Invalid username or password"
-        actual_error_msg=login_pg.get_error_msg()
+        # print(f"Verifying invalid login and Page Title")
+        with allure.step("Verifying invalid Login with error message"):
+            expected_error_msg="*Invalid username or password"
+            actual_error_msg=login_pg.get_error_msg()
 
-        assert actual_error_msg == expected_error_msg, f"Actual error message {actual_error_msg} did not match expected error message"
-        assert not login_pg.is_login_successful(), f"User should not be logged in with invalid credentials"
+            assert actual_error_msg == expected_error_msg, f"Actual error message {actual_error_msg} did not match expected error message"
+            assert not login_pg.is_login_successful(), f"User should not be logged in with invalid credentials"
